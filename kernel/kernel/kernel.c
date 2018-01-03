41517c73046b867666204d81c4bd6ee43ba9e8f9
Layton Wang <wangzixu.china@gmail.com>
2018-01-03 18:29:15 +0800

    Implement initial Stack Smashing Protector

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
+void foo(const char* str) {
+  char buffer[8];
+  memcpy(buffer, str, 10);
+}
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
@@ -11,4 +17,6 @@ void kernel_main(void) {
 #ifdef _KERNEL_DEBUG
   printf("[INFO] Kernel exiting.\n");
 #endif
+  printf("Test SSP.\n");
+  foo("This is a test of SSP.");
 }
