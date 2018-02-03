67ca9e83f8b62d143b6a32fe6c935ba3d4e7805b
Layton Wang <wangzixu.china@gmail.com>
2018-02-03 21:35:38 +0800

    Start working on memory management

--- kernel/include/kernel/memory.h
+++ kernel/include/kernel/memory.h
@@ -2,15 +2,50 @@
 #define _KERNEL_MEMORY_H
 
 #include <stdint.h>
+#include <stddef.h>
 #include <kernel/multiboot.h>
 
 #define NUM_PDE 1024
 #define NUM_PTE 1024
+#define PDE_PRST 0x1
+#define PDE_RW   0x2
+#define PDE_US   0x4
+#define PDE_PWT  0x8
+#define PDE_PCD  0x10
+#define PDE_ACCS 0x20
+#define PDE_DIRT 0x40
+#define PDE_PGSZ 0x80
+#define PDE_GL   0x100
+#define PDE_PAT  0x200
+#define PTE_PRST 0x1
+#define PTE_RW   0x2
+#define PTE_US   0x4
+#define PTE_PWT  0x8
+#define PTE_PCD  0x10
+#define PTE_ACCS 0x20
+#define PTE_DIRT 0x40
+#define PTE_PAT  0x80
+#define PTE_GL   0x100
+#define MAP_PGSZ 0x8000
 
 typedef uint32_t mem_addr_t;
 typedef uint32_t pde_t;
 typedef uint32_t pte_t;
 
+struct avail_phys_mem_node {
+  struct avail_phys_mem_node* prev;
+  struct avail_phys_mem_node* next;
+  size_t size;
+};
+
+typedef struct avail_phys_mem_node avail_phys_mem_node_t;
+
+static inline void flush_tlb(mem_addr_t addr) {
+  __asm__ __volatile__ ("invlpg (%0)" :: "r" (addr) : "memory");
+}
+
 void init_mem(multiboot_info_t*);
+mem_addr_t get_phys_addr(mem_addr_t);
+void map_page(mem_addr_t, mem_addr_t, uint16_t);
 
 #endif
--- kernel/kernel/memory.c
+++ kernel/kernel/memory.c
@@ -47,20 +47,63 @@ void init_mem(multiboot_info_t* mbi) {
   );
 #endif
   }
+  mem_addr_t page_directory_addr;
+  __asm__ __volatile__ ("mov %%cr3, %0" : "=r" (page_directory_addr));
+  page_directory = (pde_t*) (page_directory_addr + 0xC0000000);
 #ifdef _KERNEL_DEBUG
   terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
   printf("[INFO] ");
   terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK));
   printf("Memory environments initialized.\n");
-  mem_addr_t page_directory_addr;
-  __asm__ __volatile__ ("mov %%cr3, %0" : "=r" (page_directory_addr));
   terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
   printf("[INFO] ");
   terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK));
   printf("Page directory at 0x%08x (PHY).\n", page_directory_addr);
-  page_directory = (pde_t*) (page_directory_addr + 0xC0000000);
-  printf("page_directory[0]    = 0x%08x.\n", page_directory[0]);
-  printf("page_directory[768]  = 0x%08x.\n", page_directory[768]);
-  printf("page_directory[1023] = 0x%08x.\n", page_directory[NUM_PDE - 1]);
 #endif
+}
+
+mem_addr_t get_phys_addr(mem_addr_t virt_addr) {
+  size_t page_dir_index = virt_addr >> 22;
+  pde_t pde = page_directory[page_dir_index];
+#ifdef _KERNEL_DEBUG
+  terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK));
+  printf("[INFO] ");
+  terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK));
+  printf("Getting physical address for 0x%08x.\n", virt_addr);
+  printf("    page_dir_index = %d\n", page_dir_index);
+  printf("    page_dir_entry = 0x%08x\n", pde);
+#endif
+  if (pde | PDE_PGSZ) {  // 4MB page
+#ifdef _KERNEL_DEBUG
+  printf("    (4M page) phys_addr = 0x%08x\n",
+    (pde & ~0x003FFFFF) | (virt_addr & 0x003FFFFF));
+#endif
+    return (pde & ~0x003FFFFF) | (virt_addr & 0x003FFFFF);
+  } else {  // 4KB page
+    pte_t* page_table = (pte_t*) (pde >> 12);
+    size_t page_tab_index = (virt_addr >> 12) & 0x03FF;
+    pte_t pte = page_table[page_tab_index];
+#ifdef _KERNEL_DEBUG
+  printf("    (4K page) page_table     = %0x08x\n", (mem_addr_t) page_table);
+  printf("              page_tab_index = %d\n", page_tab_index);
+  printf("              page_tab_entry = 0x%08x\n", pte);
+  printf("              phys_addr      = 0x%08x\n",
+    (pte & ~0x0FFF) | (virt_addr & 0x0FFF));
+#endif
+    return (pte & ~0x0FFF) | (virt_addr & 0x0FFF);
+  }
+}
+
+void map_page(mem_addr_t phys_addr, mem_addr_t virt_addr, uint16_t flags) {
+  // TODO: 4K align addresses.
+  if (flags | MAP_PGSZ) {  // Map 4M page.
+    size_t page_dir_index = virt_addr >> 22;
+    pde_t pde = 0;
+    pde |= phys_addr & ~0x003FFFFF;
+    pde |= flags & 0x1FFF;
+    page_directory[page_dir_index] = pde | PDE_PRST;
+    flush_tlb(phys_addr);
+  } else {  // Map 4K page.
+    // TODO
+  }
 }
